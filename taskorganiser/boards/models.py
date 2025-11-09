cc69c0d46c92577ad3befeccc3f03e43b9515bf0
Henryk Greysson <techbossh19@gmail.com>
2025-11-10 08:52:02 +1100

    hi

--- taskorganiser/boards/models.py
+++ taskorganiser/boards/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boards')
+
+    def __str__(self):
+        return self.name
+    
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='groups')
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name}, {self.board.name}"
+    
+class Task(models.Model):
+    content = models.TextField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='tasks')
+    completed = models.BooleanField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.content}"
